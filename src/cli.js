2547cb9d583ebcc15f5537be8c28a2ae5fa4b217
Spiro Metaxas <spirometaxas@gmail.com>
2022-12-30 21:22:57 -0500

    Version 1.1.0

--- src/cli.js
+++ src/cli.js
@@ -1,32 +1,89 @@
 #!/usr/bin/env node
 const h_tree = require('./index.js');
 
-const printUsage = function() {
-    console.log('\nUsage:\n' + '  $ h-tree-cli <n>\n' + '  $ h-tree-cli <n> <size>\n');
+const printUsage = function(showIntro) {
+    if (showIntro) {
+        console.log(h_tree.create(7));
+        console.log(' Print the H-Tree Fractal to the console!');
+    }
+    console.log('\n' + 
+                ' Usage:\n' + 
+                '   $ h-tree-cli <n>\n' + 
+                '   $ h-tree-cli <n> <size>\n' + 
+                '\n' + 
+                '   <n> is the recursive step, a number greater than or equal to 0\n' + 
+                '   <size> is the size to draw, a number greater than or equal to <n>\n' + 
+                '\n' +
+                ' Options:\n' + 
+                '   --line=<line>  Draw using a specific line type: [bold|double|standard]\n');
+}
+
+const getFlags = function(params) {
+    let flags = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (params[i].startsWith('-')) {
+                flags.push(params[i]);
+            }
+        }
+    }
+    return flags;
+}
+
+const getValues = function(params) {
+    let values = [];
+    if (params) {
+        for (let i = 0; i < params.length; i++) {
+            if (!params[i].startsWith('-')) {
+                values.push(params[i]);
+            }
+        }
+    }
+    return values;
+}
+
+const getLine = function(flags) {
+    for (let i = 0; i < flags.length; i++) {
+        if (flags[i] && flags[i].toLowerCase().startsWith('--line=')) {
+            const line = flags[i].substring(7);
+            if (line) {
+                if (line.toLowerCase() === 'bold' || line.toLowerCase() === 'double' || line.toLowerCase() === 'standard') {
+                    return line.toLowerCase();
+                } else {
+                    console.log('\n Warning: Please provide a supported line type: [bold|double|standard]');
+                }
+            } else {
+                console.log('\n Warning: Please provide a supported line type: [bold|double|standard]');
+            }
+        }
+    }
+    return undefined;
 }
 
 if (process.argv.length > 2) {
     const params = process.argv.slice(2);
-    if (params[0] && !isNaN(params[0]) && parseInt(params[0]) >= 0) {
-        var n = parseInt(params[0]);
+    const values = getValues(params);
+    const flags = getFlags(params);
+    if (values[0] && !isNaN(values[0]) && parseInt(values[0]) >= 0) {
+        var n = parseInt(values[0]);
         var s = undefined;
-        if (params[1]) {
-            if (!isNaN(params[1]) && parseInt(params[1]) >= n) {
-                s = parseInt(params[1]);
+        if (values[1]) {
+            if (!isNaN(values[1]) && parseInt(values[1]) >= n) {
+                s = parseInt(values[1]);
             } else {
-                console.log('\n<size> should be a number greater than or equal to <n>');
+                console.log('\n <size> should be a number greater than or equal to <n>');
                 printUsage();
             }
         } else {
             s = n;
         }
         if (n !== undefined && s !== undefined) {
-            console.log(h_tree.create(n, s));
+            console.log(h_tree.create(n, { size: s, line: getLine(flags) }));
         }
     } else {
-        console.log('\n<n> should be a number greater than or equal to 0');
-        printUsage();
+        console.log('\n <n> should be a number greater than or equal to 0');
+        printUsage(false);
     }
 } else {
-    printUsage();
+    printUsage(true);
 }
--- src/index.js
+++ src/index.js
@@ -20,6 +20,81 @@ const getHeight = function(n) {
   }
 }
 
+const LINES = {
+  '─': {
+    STANDARD: '─',
+    BOLD: '━',
+    DOUBLE: '═',
+  },
+  '│': {
+    STANDARD: '│',
+    BOLD: '┃',
+    DOUBLE: '║',
+  },
+  '┌': {
+    STANDARD: '┌',
+    BOLD: '┏',
+    DOUBLE: '╔',
+  },
+  '┐': {
+    STANDARD: '┐',
+    BOLD: '┓',
+    DOUBLE: '╗',
+  },
+  '┘': {
+    STANDARD: '┘',
+    BOLD: '┛',
+    DOUBLE: '╝',
+  },
+  '└': {
+    STANDARD: '└',
+    BOLD: '┗',
+    DOUBLE: '╚',
+  },
+  '┼': {
+    STANDARD: '┼',
+    BOLD: '╋',
+    DOUBLE: '╬',
+  },
+  '├': {
+    STANDARD: '├',
+    BOLD: '┣',
+    DOUBLE: '╠',
+  },
+  '┤': {
+    STANDARD: '┤',
+    BOLD: '┫',
+    DOUBLE: '╣',
+  },
+  '┬': {
+    STANDARD: '┬',
+    BOLD: '┳',
+    DOUBLE: '╦',
+  },
+  '┴': {
+    STANDARD: '┴',
+    BOLD: '┻',
+    DOUBLE: '╩',
+  }
+};
+
+const getLine = function(lineId, lineType) {
+  if (LINES[lineId] !== undefined && lineType !== undefined) {
+    return LINES[lineId][lineType.toUpperCase()];
+  } else if (LINES[lineId] !== undefined) {
+    return LINES[lineId].STANDARD;
+  } else {
+    return ' ';
+  }
+}
+
+const getLineType = function(line) {
+  if (line !== undefined && (line.toLowerCase() === 'standard' || line.toLowerCase() === 'double' || line.toLowerCase() === 'bold')) {
+    return line.toLowerCase();
+  }
+  return 'standard';
+}
+
 const createBoard = function(w, h) {
   let board = [];
   for (let i = 0; i < h; i++) {
@@ -32,10 +107,10 @@ const createBoard = function(w, h) {
   return board;
 }
 
-const drawLine = function(board, pos, scale, type) {
+const drawLine = function(board, pos, size, type) {
   if (type === 'up' || type === 'down') {
-    const length = getHeight(scale);
-    var startX = pos.x - parseInt(getHeight(scale) / 2);
+    const length = getHeight(size);
+    var startX = pos.x - parseInt(getHeight(size) / 2);
     for (let i = 0; i < length; i++) {
       if (type === 'up' && i === parseInt(length / 2)) {
         board[pos.y][startX + i] = '┬';
@@ -46,8 +121,8 @@ const drawLine = function(board, pos, scale, type) {
       }
     }
   } else if (type === 'left' || type === 'right') {
-    const length = getHeight(scale - 1);
-    var startY = pos.y - parseInt(getHeight(scale - 1) / 2);
+    const length = getHeight(size - 1);
+    var startY = pos.y - parseInt(getHeight(size - 1) / 2);
     for (let i = 0; i < length; i++) {
       if (type === 'left' && i === parseInt(length / 2)) {
         board[startY + i][pos.x] = '├';
@@ -58,51 +133,55 @@ const drawLine = function(board, pos, scale, type) {
       }
     }
   } else {
-    const length = getHeight(scale);
-    var startX = pos.x - parseInt(getHeight(scale) / 2);
+    const length = getHeight(size);
+    var startX = pos.x - parseInt(getHeight(size) / 2);
     for (let i = 0; i < length; i++) {
       board[pos.y][startX + i] = '─';
     }
   }
 }
 
-const htree = function(n, scale, board, pos, type) {
-  drawLine(board, pos, scale, type);
+const htree = function(n, size, board, pos, type) {
+  drawLine(board, pos, size, type);
 
   if (n > 0) {
     if (type === undefined || type === 'up' || type === 'down') {
-      htree(n - 1, scale - 1, board, { x: pos.x - (parseInt(getHeight(scale) / 2)) - 1, y: pos.y }, 'left');
-      htree(n - 1, scale - 1, board, { x: pos.x + (parseInt(getHeight(scale) / 2)) + 1, y: pos.y }, 'right');
+      htree(n - 1, size - 1, board, { x: pos.x - (parseInt(getHeight(size) / 2)) - 1, y: pos.y }, 'left');
+      htree(n - 1, size - 1, board, { x: pos.x + (parseInt(getHeight(size) / 2)) + 1, y: pos.y }, 'right');
     } else {
-      htree(n - 1, scale - 1, board, { x: pos.x, y: pos.y - (parseInt(getHeight(scale - 1) / 2)) - 1 }, 'up');
-      htree(n - 1, scale - 1, board, { x: pos.x, y: pos.y + (parseInt(getHeight(scale - 1) / 2)) + 1 }, 'down');
+      htree(n - 1, size - 1, board, { x: pos.x, y: pos.y - (parseInt(getHeight(size - 1) / 2)) - 1 }, 'up');
+      htree(n - 1, size - 1, board, { x: pos.x, y: pos.y + (parseInt(getHeight(size - 1) / 2)) + 1 }, 'down');
     }
   }
 }
 
-const draw = function(board) {
+const draw = function(board, lineType) {
   var result = '\n ';
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      result += board[i][j];
+      result += getLine(board[i][j], lineType);
     }
     result += '\n ';
   }
   return result;
 }
 
-const create = function(n, scale) {
+const create = function(n, config) {
   if (n === undefined || n < 0) {
     return '';
   }
-  if (scale === undefined || scale < n) {
-    scale = n;
+  
+  let size = n;
+  if (config && config.size && config.size > n) {
+    size = config.size;
   }
 
-  const board = createBoard(getWidth(scale), getHeight(scale)); 
-  htree(n, scale, board, { x: parseInt(getWidth(scale) / 2.0), y: parseInt(getHeight(scale) / 2.0) }); 
+  const lineType = config !== undefined ? getLineType(config.line) : undefined;
 
-  return draw(board);
+  const board = createBoard(getWidth(size), getHeight(size)); 
+  htree(n, size, board, { x: parseInt(getWidth(size) / 2.0), y: parseInt(getHeight(size) / 2.0) }); 
+
+  return draw(board, lineType);
 }
 
 module.exports = {
